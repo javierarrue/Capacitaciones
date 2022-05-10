63fb37f00a92df9e1e0e5726890a16f1b2f23230
unknown <soporte@GRUSSO.amp.intranet.gob.pa>
2022-05-10 10:59:56 -0500

    Refactorizando

--- js/register.js
+++ js/register.js
@@ -1,59 +1,82 @@
 const addFieldBtn = document.querySelector('.addFieldBtn');
 const deleteFieldBtn = document.querySelector('.deleteFieldBtn');
 const coursesDivWrapper = document.querySelector('.cursos');
-//Variable inputCounter, necesaria para diferenciar la lista de input's posibles a generar.
+
+//Variable inputCounter, necesaria para diferenciar un input de otro.
 //inputCounter variable, needed to differentiate one input to another
 var inputCounter = 0;
+//HTML elements
+var textInput, rowDiv, inputDiv, btnDiv, btnDeleteField; 
 
 addFieldBtn.addEventListener('click', () => {
-    createInput();
+    createHtmlElements();
+    setInputType();
+    setValues();
+    setAttributes();
+    setClassNames();
+    configureBtnDelete();
+    //Juntando/anexando elementos en uno solo elemento HTML
+    appendChilds();
+    inputCounter++;
 })
 
-function createInput(){
-    var input = document.createElement('input');
-    
-    var rowDiv = document.createElement('div');
-    var inputDiv = document.createElement('div');
-    var btnDiv = document.createElement('div');
+function createHtmlElements(){
+    textInput = document.createElement('input');
+    rowDiv = document.createElement('div');
+    inputDiv = document.createElement('div');
+    btnDiv = document.createElement('div');
+    btnDeleteField = document.createElement('button');
+}
 
-    var btnDeleteField = document.createElement('button');
-    var symbol = document.createTextNode('-');
+function setInputType(){
+    textInput.type = 'text';
+}
 
-    input.type = 'text';
-    input.className = "form-control"
-    //Si se quiere agregar campos editables comentar las dos lineas de abajo.
-    input.value = document.querySelector('.cursoSugeridoText').value;
-    input.setAttribute('readonly','');
+function setValues(){
+    //Para campos editables eliminar linea de abajo
+    textInput.value = document.querySelector('.cursoSugeridoText').value;
+}
 
-    input.setAttribute('name','cursoSugerido'+inputCounter);
-    
+function setAttributes(){
+    //Para campos editables eliminar linea de abajo
+    textInput.setAttribute('readonly','');
+    textInput.setAttribute('name','cursoSugerido'+inputCounter);
+}
+
+function setClassNames(){
     //Setting the inputCounter into the className of the input, so that is unique from each other.
     //Asignando el inputCounter al className del input, para que sea unico uno del otro. 
-    rowDiv.className = 'row align-items-center mb-3 cursoSugerido'+inputCounter
-    inputDiv.className = 'col-md-10 col-sm-10'
-    btnDiv.className = 'col-md-2 col-sm-2'
+    rowDiv.className= 'row align-items-center mb-3 cursoSugerido'+inputCounter;
+    textInput.className = "form-control"
+    inputDiv.className = 'col-md-10 col-sm-10';
+    btnDiv.className = 'col-md-2 col-sm-2';
     btnDeleteField.className = 'btn btn-outline-danger deleteFieldBtn';
-    
+}
+
+//This function configures the delete button
+//Esta funcion configura el boton de eliminar un campo/input
+function configureBtnDelete(){
+    var symbol = document.createTextNode('-');
     //Set de current inputCounter to the button, for reference when deleting input
     //Definir el inputCounter actual, para hacer referencia cuando sea necesario eleminar el input.
     btnDeleteField.id = inputCounter;
     btnDeleteField.appendChild(symbol);
     btnDeleteField.type = 'button';
 
-    //Delete selected inputfield
+    //Asignando funcion onClick, para cada boton creado
     btnDeleteField.onclick = function(){
         //Getting the selected button id, to reference input to delete.
         //Obteniendo el id del boton seleccionado, para hacer referencia al input a eliminar
         var id = this.id;
         document.querySelector('.cursoSugerido'+id).remove();
     };
-    
-    inputDiv.appendChild(input);
-    btnDiv.appendChild(btnDeleteField);
+}
 
+function appendChilds(){
+    inputDiv.appendChild(textInput);
+    btnDiv.appendChild(btnDeleteField);
     rowDiv.appendChild(inputDiv);
     rowDiv.appendChild(btnDiv);
+    //Asignandolo al div ya existente dentro del HTML (rowDiv -> coursesDivWrapper)
     coursesDivWrapper.appendChild(rowDiv);
-
-    inputCounter++;
 }
