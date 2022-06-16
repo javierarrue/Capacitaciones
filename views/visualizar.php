c4f5c518a7916d54a9eaeda02f189a08a6173ada
javierarrue <armandoutp1@gmail.com>
2022-06-16 12:41:54 -0500

    Rediseñando visualizar cargos y trabajdores

--- views/visualizar.php
+++ views/visualizar.php
@@ -12,7 +12,7 @@
 
               <div class="row align-items-center">
                 <div class="col-md-10 col-sm-6">
-                  Visualizar direcciones
+                  Visualizar cargos y trabajadores
                 </div>
                 <div class="col-md-2 col-sm-6 text-center">
                   <img src="../resources/AMP-LOGO-2-01.png" alt="" class="img-fluid " style="width:5rem;">
@@ -22,13 +22,47 @@
             </div>
           </div>
         </div>
+        
+        <div class="row mb-2">
+            <div class="col-md-12">
+              <div class="card">
+                  <div class="card-header fw-bold">
+                    Seleccione los datos solicitados
+                  </div>
+                  <div class="card-body">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <label for="direccion" class="form-label">Seleccione una direccion</label>
+                          <select name="direccion" class="form-select" aria-label="Default select example" id="direccion">
+                            <option selected>Dirección</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                        </div>
+      
+                        <div class="col-md-6">
+                          <label for="departamento" class="form-label">Seleccione un departamento</label>
+                          <select name="departamento" class="form-select" aria-label="Default select example" id="departamento">
+                            <option selected>Departamento</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                        </div>
+                      </div>
+
+                    </div>
+                </div>
+              </div>
+          </div>
 
         <div class="row">
           <div class="col-md-12">
             <div class="card">
               <div class="card-header">
                 <div class="card-header">
-                  Data tables
+                  Lista de trabajadores
                 </div>
                 <div class="card-body">
                   <div class="table-responsive">
@@ -38,33 +72,47 @@
                     style="width: 100%">
                     <thead>
                       <tr>
-                        <th>Dirección</th>
-                        <th>N° de departamentos</th>
-                        <th>N° de trabajadores</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Cédula</th>
+                        <th>Cargo</th>
+                        <th>Acción</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                          <td> <a href="visualizar_departamentos.php">Marina Mercante</a> </td>
-                          <td>5</td>
-                          <td>20</td>
+                          <td>Javier</td>
+                          <td>Arrue</td>
+                          <td>8-941-1079</td>
+                          <td>Analista</td>
+                          <td>
+                            <form action="visualizar_detalles.php">
+                              <button type="submit" class="text-primary action-btn"><i class="bi bi-search"></i></button>
+                            </form>
+                          </td>
                       </tr>
                       <tr>
-                          <td>Gente de Mar</td>
-                          <td>2</td>
-                          <td>63</td>
+                        <td>Carlos</td>
+                          <td>Gonzalez</td>
+                          <td>8-190-1009</td>
+                          <td>Jefe</td>
+                          <td><button type="button" class="text-primary action-btn"><i class="bi bi-search"></i></button></td>
                       </tr>     
                       <tr>
-                          <td>Puertos</td>
-                          <td>1</td>
-                          <td>33</td>
+                        <td>Camila</td>
+                          <td>Torres</td>
+                          <td>8-993-231</td>
+                          <td>Analista</td>
+                          <td><button type="button" class="text-primary action-btn"><i class="bi bi-search"></i></button></td>
                       </tr>
                     </tbody>
                     <tfoot>
                       <tr>
-                        <th>Área</th>
-                        <th>N° de departamentos</th>
-                        <th>N° de trabajadores</th>
+                      <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Cédula</th>
+                        <th>Cargo</th>
+                        <th>Acción</th>
                       </tr>
                     </tfoot>
                   </table>
