baa107b6d41fc9000df05fef89f7c85e12a9072a
javierarrue <armandoutp1@gmail.com>
2022-06-17 15:09:01 -0500

    Mejorando diseño de Registrar curso requerido

--- views/registrar_requerido.php
+++ views/registrar_requerido.php
@@ -20,7 +20,7 @@
         <?php }?>
         <div class="row">
           <div class="col-md-12 fw-bold fs-3">
-            <div class="card">
+            <div class="card shadow mb-2">
               <div class="card-body">
                 <div class="row align-items-center">
                   <div class="col-md-10 col-sm-6">
@@ -34,9 +34,52 @@
             </div>
           </div>
         </div>
+        <!-- Formulario direccion - departamento -->
+        <div class="row mb-2 mb-2">
+          <div class="col-md-12">
+            <div class="card shadow">
+              <div class="card-header fw-bold">
+                Seleccione los datos solicitados
+              </div>
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-md-4">
+                    <label for="direccion" class="form-label">Seleccione una direccion</label>
+                    <select name="direccion" class="form-select" aria-label="Default select example" id="direccion">
+                      <option selected>Dirección</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                  <div class="col-md-4">
+                    <label for="departamento" class="form-label">Seleccione un departamento</label>
+                    <select name="departamento" class="form-select" aria-label="Default select example" id="departamento">
+                      <option selected>Departamento</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                  <div class="col-md-4">
+                    <label for="departamento" class="form-label">Seleccione un cargo</label>
+                    <select name="departamento" class="form-select" aria-label="Default select example" id="departamento">
+                      <option selected>Cargo</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /Formulario direccion - departamento -->
+
         <!-- REGISTRAR NUEVO USUARIO -->
           <!-- Modal -->
-          <div class="modal fade" id="nuevoUsuario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="nuevoUsuario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
             <div class="modal-content">
               <form action="../includes/nuevoCursoRequerido.inc.php" method="POST">
@@ -107,14 +150,20 @@
         <!-- /REGISTRAR NUEVO USUARIO -->
         <div class="row">
           <div class="col-md-12">
-            <div class="card">
-              <div class="card-header">
+            <div class="card shadow">
                 <div class="card-header">
-                  <!-- Button trigger modal -->
-                  <button type="button" class="mt-2 btn register-btn text-white" data-bs-toggle="modal" data-bs-target="#nuevoUsuario">
-                    <i class="bi bi-plus-lg"></i>
-                    Nuevo curso requerido
-                  </button>
+                  <div class="row align-items-center">
+                    <div class="col-6">
+                      <span class="fw-bold">Cursos requeridos</span>
+                    </div>
+                    <div class="col-6 text-end">
+                      <!-- Button trigger modal -->
+                      <button type="button" class="mt-2 btn register-btn text-white" data-bs-toggle="modal" data-bs-target="#nuevoUsuario">
+                        <i class="bi bi-plus-lg"></i>
+                        Nuevo curso requerido
+                      </button>
+                    </div>
+                  </div>
                 </div>
                 <div class="card-body">
                   <div class="row">
@@ -122,13 +171,12 @@
                     <div class="table-responsive">
                       <table
                         id="example"
-                        class="table table-striped data-table"
+                        class="table table-hover data-table"
                         style="width: 100%">
                         <thead>
                           <tr>
                             <th>Curso</th>
-                            <th>Editar</th>
-                            <th>Eliminar</th>
+                            <th class="text-center">Acción</th>
                           </tr>
                         </thead>
                         <tbody>
@@ -136,80 +184,75 @@
                           while($curso = $cursos_requeridos->fetch(PDO::FETCH_ASSOC)){?>
                           <tr>
                             <td><?php echo $curso["name"]; ?></td>
-                            
-                            <!-- EDITAR USUARIO -->
-                            <td>
-                              <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#editarUsuario_<?php echo $curso["id"]; ?>">
+                            <td class="text-center">
+                              <button type="button" class="btn text-primary action-btn" data-bs-toggle="modal" data-bs-target="#editarUsuario_<?php echo $curso["id"]; ?>">
                                 <i class="bi bi-pencil-square"></i>
                               </button>
-                                <!-- Modal -->
-                              <div class="modal fade" id="editarUsuario_<?php echo $curso["id"]; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                  <div class="modal-content">
-                                    <div class="modal-header">
-                                      <h5 class="modal-title" id="exampleModalLabel">Editar curso: <?php echo $curso["name"]?></h5>
-                                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                      <!-- EDITAR DATOS -->
-                                      <div class="tab-pane fade show active" id="user-tab-<?php echo $curso["id"]?>" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
-                                        <?php if(isset($_GET["errorEdit"])){?>
-                                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                          <strong><?php echo $_GET["errorEdit"]?></strong>
-                                          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                        </div>
-                                        <?php } ?>
-                                        <form action="../includes/editUser.inc.php" method="POST">
-                                          <div class="row mt-2">
-                                            <div class="col-12">
-                                              Nombre del curso
-                                              <div class="mb-3">
-                                                <input name="firstname" class="form-control" type="text" value="<?php echo $curso["name"]; ?>" aria-label="readonly input example">
-                                              </div>
-                                            </div>
-                                          </div>
-                                          <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                            <input type="hidden" name="id_curso" value="<?php echo $curso["id"]?>">
-                                            <input type="hidden" name="cursoNombre_old" value="<?php echo $curso["name"]?>">
-                                            <button type="submit" class="btn btn-primary" name="submit">Guardar Cambios</button>
-                                          </div>
-                                        </form>
-                                      </div>
-                                      <!--/EDITAR DATOS -->
-                                    </div>
-                                  </div>
-                                </div> 
-                              </div>
-                            </td>
-                            <!-- /EDITAR USUARIO -->
-
-                            <!-- ELIMINAR USUARIO -->
-                            <td>
-                              <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#delete_<?php echo $curso["id"]; ?>">
+                              <button type="button" class="btn text-danger action-btn" data-bs-toggle="modal" data-bs-target="#delete_<?php echo $curso["id"]; ?>">
                                   <i class="bi bi-trash3-fill"></i>
                               </button>
                                 <!-- Modal -->
-                              <div class="modal fade" id="delete_<?php echo $curso["id"]; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                  <div class="modal-content">
-                                    <div class="modal-header">
-                                      <h5 class="modal-title" id="exampleModalLabel">¿Desea eliminar el curso <?php echo $curso["name"]; ?>?</h5>
-                                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </td>
+                          </tr>
+                          
+                          <!-- EDITAR -->
+                          <div class="modal fade" id="editarUsuario_<?php echo $curso["id"]; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title" id="exampleModalLabel">Editar curso: <?php echo $curso["name"]?></h5>
+                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                  <!-- EDITAR DATOS -->
+                                  <div class="tab-pane fade show active" id="user-tab-<?php echo $curso["id"]?>" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+                                  <?php if(isset($_GET["errorEdit"])){?>
+                                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                      <strong><?php echo $_GET["errorEdit"]?></strong>
+                                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                     </div>
-                                    <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                      <form action="../includes/deleteUser.inc.php" method="POST">
-                                        <button value="<?php echo $curso["name"]; ?>" class="btn btn-danger" name="cursoNombre" type="submit">Eliminar</button>
-                                      </form>
-                                    </div>
+                                    <?php } ?>
+                                    <form action="../includes/editUser.inc.php" method="POST">
+                                      <div class="row mt-2">
+                                        <div class="col-12">
+                                          Nombre del curso
+                                          <div class="mb-3">
+                                            <input name="firstname" class="form-control" type="text" value="<?php echo $curso["name"]; ?>" aria-label="readonly input example">
+                                          </div>
+                                        </div>
+                                      </div>
+                                      <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                        <input type="hidden" name="id_curso" value="<?php echo $curso["id"]?>">
+                                        <input type="hidden" name="cursoNombre_old" value="<?php echo $curso["name"]?>">
+                                        <button type="submit" class="btn btn-primary" name="submit">Guardar Cambios</button>
+                                      </div>
+                                    </form>
                                   </div>
+                                  <!--/EDITAR DATOS -->
                                 </div>
                               </div>
-                            </td>
-                            <!-- /ELIMINAR USUARIO -->
-
-                          </tr>
+                            </div> 
+                          </div>
+                          <!-- EDITAR -->
+                          <!-- ELIMINAR -->
+                          <div class="modal fade" id="delete_<?php echo $curso["id"]; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title" id="exampleModalLabel">¿Desea eliminar el curso <?php echo $curso["name"]; ?>?</h5>
+                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                  <form action="../includes/deleteUser.inc.php" method="POST">
+                                    <button value="<?php echo $curso["name"]; ?>" class="btn btn-danger" name="cursoNombre" type="submit">Eliminar</button>
+                                  </form>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <!-- /ELIMINAR -->
                           <?php }?>
                         </tbody>
                       </table>
@@ -217,7 +260,6 @@
                     <!--/ TABLA -->
                   </div>
                 </div>
-              </div>
             </div>
           </div>
         </div>
