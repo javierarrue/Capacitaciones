94acff68275ffa0cc6682f8d621f1eb3ebdc63d3
javierarrue <armandoutp1@gmail.com>
2022-06-22 08:42:28 -0500

    Cambio, color de boton

--- views/visualizar_detalles.php
+++ views/visualizar_detalles.php
@@ -300,7 +300,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary">Guardar cambios</button>
+        <button type="button" class="btn btn-primary register-btn">Guardar cambios</button>
       </div>
     </div>
   </div>
