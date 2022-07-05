9dd1ab28604179e100a03c39b7f28ab6abe842c5
javierarrue <armandoutp1@gmail.com>
2022-07-05 15:06:09 -0500

    Conexion a nueva bd

--- includes/config.php
+++ includes/config.php
@@ -3,3 +3,8 @@
     define("USER", "root");
     define("PASSWORD", "");
     define("DB", "pruebas1");
+
+    define("HOST_mssql", "file-server");
+    define("USER_mssql", "soporte");
+    define("PASSWORD_mssql", "soporte");
+    define("DB_mssql", "OIRH");
