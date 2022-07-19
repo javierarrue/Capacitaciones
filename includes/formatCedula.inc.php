1ca53124aa0b3bb458c54b0cfeb432e44fc3e8f3
javierarrue <armandoutp1@gmail.com>
2022-07-19 13:18:21 -0500

    haciendo logica de expresiones regulares

--- includes/formatCedula.inc.php
+++ includes/formatCedula.inc.php
@@ -6,3 +6,38 @@ $pattern = "/ +(0)|0/";
 echo preg_replace($pattern, '-',$cedulaNoFormat);
 
 ///^P$|^(?:PE|E|N|[23456789]|[23456789](?:A|P)?|1[0123]?|1[0123]?(?:A|P)?)$|^(?:PE|E|N|[23456789]|[23456789](?:AV|PI)?|1[0123]?|1[0123]?(?:AV|PI)?)-?$|^(?:PE|E|N|[23456789](?:AV|PI)?|1[0123]?(?:AV|PI)?)-(?:\d{1,4})-?$|^(PE|E|N|[23456789](?:AV|PI)?|1[0123]?(?:AV|PI)?)-(\d{1,4})-(\d{1,6})$/i
+
+
+//(?:(0)?(\d{1,2})?)(  )?(PE)?(N)?( )?(?:(0{1,3})?(\d{1,4}))(?:(0{1,4})?(\d{1,5}))
+
+/**
+ * 
+ * 12  194101079
+ * N 001900548
+ * PE000190403
+ * 08  094101079
+ */
+
+/**
+Regular (provincia-libro-tomo). Ej: 1-1234-12345
+Panameño nacido en el extranjero (PE-libro-tomo). Ej: PE-1234-123456
+Extranjero con cédula (E-libro-tomo). Ej: E-1234-12345
+Naturalizado (N-libro-tomo). Ej: N-1234-12345
+Panameños nacidos antes de la vigencia (provinciaAV-libro-tomo). Ej: 1AV-1234-12345
+Población indigena (provinciaPI-libro-tomo). Ej: 1PI-1234-12345
+
+/**
+ * Patron para primera parte de la cedula (provincia|PE|E|1PI|1AV):
+ * ^(?:(0)?(\d{1,2})?)?(  )?(PE)?(N)?(E)?( )?(1PI)?(1AV)?
+ * O un poco mas limpio
+ * ^(\d{2})?(  )?(PE)?(N)?(E)?( )?(1PI)?(1AV)?
+ * 
+ * Patron para sacar el libro (4 digitos) y tomo (5 digitos) de la cedula.
+ * (\d{4})(\d{5})
+ *
+ * Patron para el libro 
+ * (0{1,3})?(\d{1,4})
+ * 
+ * Patron para tomo
+ * (0{1,4})?(\d{1,5})
+ */
